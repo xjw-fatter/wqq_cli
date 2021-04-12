9d9d4bd16b3338093907c75c34fe53495be730a7
肥起来的xjw <1055271311@qq.com>
2021-04-12 18:43:47 +0800

    '新增是否安装依赖选项'

--- commands/init.js
+++ commands/init.js
@@ -6,6 +6,7 @@ const chalk = require("chalk"); // 颜色
 const ora = require("ora");   // loading
 const inquirer = require("inquirer"); // 交互式命令行用户界面集合
 const tplNames = Object.keys(config.tpl);
+const path = require("path");
 
 module.exports = () => {
 	co(function* () {
@@ -27,13 +28,25 @@ module.exports = () => {
 					return true;
 				},
 			},
+			{
+				type: "input",
+				message: "是否安装依赖Y/N",
+				name: "needInstall",
+				validate(val) {
+					if (!val || (val != "Y" && val != "N") ) {
+						return "请输入Y/N";
+					}
+					return true;
+				},
+			},
 		];
 
 		inquirer.prompt(promptList).then((answers) => {
-			let ind = config.tpl[answers.tplName];
+			let { tplName, projectName, needInstall} = answers;
+			let ind = config.tpl[tplName];
 			let gitUrl = `${ind.url}`;
 			// git命令，远程拉取项目并自定义项目名
-			let cmdStr = `git clone ${gitUrl} ${answers.projectName} && cd ${answers.projectName} && git checkout ${ind.branch}`;
+			let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${ind.branch}`;
 			let spinner = ora("\n Start generating...");
 			spinner.start();
 			exec(cmdStr, (error, stdout, stderr) => {
@@ -43,7 +56,31 @@ module.exports = () => {
 					process.exit();
 				}
 				console.log(chalk.green("\n √ Generation completed!"));
-				console.log(chalk.blue(`\n cd ${answers.projectName} && npm install \n`));
+				if( needInstall == "Y") {
+					// console.log(`起始的目录: ${process.cwd()}`);
+					try {
+						process.chdir(projectName);
+						// console.log(`新的目录: ${process.cwd()}`);
+						let spinner = ora("Start install...");
+						spinner.start();
+						exec('npm install', (error, stdout, stderr) => {
+							spinner.stop();
+							if (error) {
+								console.log(error);
+								process.exit();
+							}
+							// console.log("stdout",stdout);
+							// console.log("stderr",stderr);
+							console.log(chalk.green(`\n √ The installation is complete \n`));
+							console.log(chalk.green(`\n cd ${projectName} && ...\n`));
+							process.exit();
+						});
+					  } catch (err) {
+						console.error(`chdir: ${err}`);
+					  }
+					return;
+				}
+				console.log(chalk.blue(`\n cd ${projectName} && npm install \n`));
 				process.exit();
 			});
 		});
